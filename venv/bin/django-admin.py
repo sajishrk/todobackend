f0b1141be58540894b7760ab9f02930236bef31c
sajish krishnan <sajishkrishnan@sajishs-MacBook-Pro.local>
2017-02-13 13:38:11 -0800

    Second Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sajishkrishnan/Desktop/docker-ansible-jenkins/todobackend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
